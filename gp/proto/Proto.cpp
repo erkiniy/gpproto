44ecef98baf68177b3454d89ce4ac6c999da7703
Jaloliddin Erkiniy <jalol@ProDigitalMacMini.local>
2018-08-10 17:55:07 +0500

    add proto

---
+++ gp/proto/Proto.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Jaloliddin Erkiniy on 8/10/18.
+//
+
+#include "Proto.h"
---
+++ install/include/gp/proto/Proto.h
@@ -0,0 +1,13 @@
+//
+// Created by Jaloliddin Erkiniy on 8/10/18.
+//
+
+#ifndef GPPROTO_PROTO_H
+#define GPPROTO_PROTO_H
+
+class Proto {
+    
+};
+
+
+#endif //GPPROTO_PROTO_H
