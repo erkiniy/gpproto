ae7a4bf3ac5b033ce85673ca40c9b2d77534ba7a
Jaloliddin Erkiniy <jalol@ProDigitalMacMini.local>
2018-08-17 21:38:45 +0500

    implement crypto/hash functions

---
+++ gputils/gp/utils/BigNum.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Jaloliddin Erkiniy on 8/17/18.
+//
+
+#include "BigNum.h"
---
+++ gputils/gp/utils/BigNum.h
@@ -0,0 +1,14 @@
+//
+// Created by Jaloliddin Erkiniy on 8/17/18.
+//
+
+#ifndef GPPROTO_BIGNUM_H
+#define GPPROTO_BIGNUM_H
+
+
+class BigNum {
+
+};
+
+
+#endif //GPPROTO_BIGNUM_H
